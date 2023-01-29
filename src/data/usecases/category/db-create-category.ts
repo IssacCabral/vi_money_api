48b205462cd858ea4892947ace559dcf8beb8910
Issac Cabral <issaccabrall@gmail.com>
2023-01-29 15:12:55 -0300

    refactor: remove unnecessary user repository

--- src/data/usecases/category/db-create-category.ts
+++ src/data/usecases/category/db-create-category.ts
@@ -1,6 +1,5 @@
 import { Inject } from '@nestjs/common';
 import { ICategoryRepository } from 'src/data/repositories/category-repository';
-import { IUserRepository } from 'src/data/repositories/user-repository';
 import { BusinessError } from 'src/domain/errors/business-error';
 import {
   CreateCategoryParams,
@@ -8,23 +7,14 @@ import {
 } from 'src/domain/types/category-params';
 import { ICreateCategoryUseCase } from 'src/domain/usecases/category/create-category';
 import { CATEGORY_REPOSITORY } from 'src/infra/modules/category/category.providers';
-import { USER_REPOSITORY } from 'src/infra/modules/user/user.providers';
 
 export class DbCreateCategoryUseCase implements ICreateCategoryUseCase {
   constructor(
     @Inject(CATEGORY_REPOSITORY)
     private categoryRepository: ICategoryRepository,
-    @Inject(USER_REPOSITORY)
-    private userRepository: IUserRepository,
   ) {}
 
   async execute(params: CreateCategoryParams): Promise<CreateCategoryReturns> {
-    const userExists = await this.userRepository.findUserById(params.userId);
-
-    if (!userExists) {
-      throw new BusinessError('User is not found', 404);
-    }
-
     const userCategoryAlreadyExists =
       await this.categoryRepository.findUserCategoryByName(
         params.userId,
--- src/infra/modules/category/controllers/create-category/usecase-module/db.usecase.module.ts
+++ src/infra/modules/category/controllers/create-category/usecase-module/db.usecase.module.ts
@@ -1,18 +1,12 @@
 import { Module, Provider } from '@nestjs/common';
 import { DatabaseModule } from 'src/infra/database/database.module';
 import { PrismaCategoryRepository } from 'src/infra/database/prisma/repositories/prisma.category.repository';
-import { PrismaUserRepository } from 'src/infra/database/prisma/repositories/prisma.user.repository';
-import { USER_REPOSITORY } from 'src/infra/modules/user/user.providers';
 import { CATEGORY_REPOSITORY } from '../../../category.providers';
 
 const providers: Provider[] = [
   {
     provide: CATEGORY_REPOSITORY,
     useClass: PrismaCategoryRepository,
-  },
-  {
-    provide: USER_REPOSITORY,
-    useClass: PrismaUserRepository,
   },
 ];
 
