df205ff0a1dbddfaba4b354a872f0a73a8220828
Issac Cabral <issaccabrall@gmail.com>
2023-02-01 08:34:55 -0300

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api/v1');
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
